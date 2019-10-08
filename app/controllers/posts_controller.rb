0762599eff6d20f238a62042bd9c2d49575d6ab7
Haroon Ahmed <haroon.ahmed25@gmail.com>
2019-10-08 18:08:02 +0100

    cleanup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    STDERR.puts "==============================="
     STDERR.puts request.env['rack.input'].read # => {"abc": "123"}
     STDERR.puts request.body # => #<File:0x00007f8460b2b088>
     STDERR.puts request.raw_post # =>
-    STDERR.puts request.body.read # => 
-    STDERR.puts "==============================="
+    STDERR.puts request.body.read # =>
   end
 end
